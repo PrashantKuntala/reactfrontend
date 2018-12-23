b37347f5668620379a37f1d0a39e794bcfc615ce
PrashantKuntala <prashantkuntala@gmail.com>
2018-12-23 16:50:08 -0500

    Remove unwanted files and check test axios requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+
+// simple click function to check the api
+ function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    axios.get('http://localhost:8080/reviewSamples')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  }
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Simple React App</h1>
+        <br/>
+        <button onClick={handleClick}>
+        Click me
+        </button>        
       </div>
     );
   }
